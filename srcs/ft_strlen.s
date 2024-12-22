59ba667b5464595949dc10b92db8561e7ba31a41
Astrid GAULTIER <asgaulti@bess-f2r6s6.clusters.42paris.fr>
2024-12-22 15:50:13 +0100

    ft_strlen.s ok

--- srcs/ft_strlen.s
+++ srcs/ft_strlen.s
@@ -1,5 +1,7 @@
 section .text
-    global ft_strlen
+	global ft_strlen
+
+; initialisation des variables:
 
 ft_strlen:
     push rbp
@@ -7,12 +9,13 @@ ft_strlen:
     xor rcx, rcx
 
 _loop:
-    cmp [rdi], byte 0  ; compare rdi to null ptr (end of str)
-    jz _end       ; si c'est 0 = fin de loop, renvoi au return
+    cmp [rdi], byte 0	; compare rdi to null ptr (end of str)
+    jz _end				; si c'est 0 = fin de loop, renvoi au return
     inc rcx
     inc rdi
     jmp _loop
 
 _end:
-    mov rax, rcx  ; deplacer valeur de rcx dans rax (registre de retour e valeurs)
-    ret
+    mov rax, rcx		; deplacer valeur de rcx dans rax (registre de retour e valeurs)
+    pop rbp				; depiler rbp 
+	ret
