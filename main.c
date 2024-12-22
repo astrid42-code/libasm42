342a2ec1a30acf1072bbfe643528f2876698aa3a
Astrid GAULTIER <asgaulti@bess-f2r6s4.clusters.42paris.fr>
2024-12-22 16:57:31 +0100

    ft_strlen.s comments

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: asgaulti <asgaulti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/10 15:30:32 by asgaulti          #+#    #+#             */
-/*   Updated: 2024/12/22 15:29:20 by asgaulti         ###   ########.fr       */
+/*   Updated: 2024/12/22 16:48:55 by asgaulti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,35 +15,50 @@
 void		ft_main_strlen(void)
 {
 	printf("-------------------ft_strlen-------------------\n");
-	printf("ft_strlen : \n");
+	printf("ft_strlen : test12345\n");
 	printf("%zd\n", ft_strlen("test12345"));
 	printf("strlen : \n");
 	printf("%zd\n", strlen("test12345"));
 	printf("----------\n");
-	printf("ft_strlen : \n");
+	printf("ft_strlen : 1\n");
 	printf("%zd\n", ft_strlen("1"));
 	printf("strlen : \n");
 	printf("%zd\n", strlen("1"));
 	printf("----------\n");
-	printf("ft_strlen : \n");
+	printf("ft_strlen : test\n");
 	printf("%zd\n", ft_strlen("test"));
 	printf("strlen : \n");
 	printf("%zd\n", strlen("test"));
 	printf("----------\n");
-	printf("ft_strlen : \n");
+	printf("ft_strlen : nothing inside quotes\n");
 	printf("%zd\n", ft_strlen(""));
 	printf("strlen : \n");
 	printf("%zd\n", strlen(""));
 	printf("----------\n");
-	printf("ft_strlen : \n");
+	printf("ft_strlen : 1 space test\n");
+	printf("%zd\n", ft_strlen(" "));
+	printf("strlen : \n");
+	printf("%zd\n", strlen(" "));
+	printf("----------\n");
+	printf("ft_strlen : 1 space test with return\n");
+	printf("%zd\n", ft_strlen(" \n"));
+	printf("strlen : \n");
+	printf("%zd\n", strlen(" \n"));
+	printf("----------\n");
+	printf("ft_strlen : return test\n");
 	printf("%zd\n", ft_strlen("\n"));
 	printf("strlen : \n");
 	printf("%zd\n", strlen("\n"));
 	printf("----------\n");
-	printf("ft_strlen : \n");
+	printf("ft_strlen : multiple characters test \n");
 	printf("%zd\n", ft_strlen("zzzzzzzzzzzzzzzzzzzzzzzz hey"));
 	printf("strlen : \n");
 	printf("%zd\n", strlen("zzzzzzzzzzzzzzzzzzzzzzzz hey"));
+	printf("----------\n");
+	printf("ft_strlen : multiple characters test with return \n");
+	printf("%zd\n", ft_strlen("zzzzzzzzzzzzzzzzzzzzzzzz hey\n"));
+	printf("strlen : \n");
+	printf("%zd\n", strlen("zzzzzzzzzzzzzzzzzzzzzzzz hey\n"));
 }
 
 int     main(void)
--- srcs/ft_strlen.s
+++ srcs/ft_strlen.s
@@ -1,19 +1,23 @@
 section .text
 	global ft_strlen
 
-; initialisation des variables:
+; variables initialization :
 
 ft_strlen:
-    push rbp
-    mov rbp, rsp
-    xor rcx, rcx
+    push rbp            ; save base pointer on stack (save local variables, for ex. : variables to use in a fct)
+    mov rbp, rsp        ; stack ptr goes in base pointer
+    xor rcx, rcx		; initialize loop counter to 0
+
+; loop while (s[i])
 
 _loop:
-    cmp [rdi], byte 0	; compare rdi to null ptr (end of str)
-    jz _end				; si c'est 0 = fin de loop, renvoi au return
-    inc rcx
-    inc rdi
-    jmp _loop
+    cmp [rdi], byte 0	; compare rdi (str[i]) to null ptr (end of str)
+    jz _end				; if null ptr = end of loop, jump to _end section and return
+    inc rcx 			; increase loop counter
+    inc rdi				; increase dest address (i++;)
+    jmp _loop			
+
+; end section, returns the final value
 
 _end:
     mov rax, rcx		; deplacer valeur de rcx dans rax (registre de retour e valeurs)
