e9907e2688e94af9ea872f4217361a8454cc3512
Astrid GAULTIER <asgaulti@bess-f3r3s4.clusters.42paris.fr>
2025-01-06 16:07:12 +0100

    ft_strdup : still pourri

--- srcs/ft_strdup.s
+++ srcs/ft_strdup.s
@@ -1,16 +1,33 @@
 section .text
-    global ft_strdup
-    extern malloc
-    extern ft_strlen
-    extern ft_strcpy
+		global ft_strdup
+		extern ft_strlen
+		extern ft_strcpy
 
 ft_strdup:
-    push rbp
-    mov rbp, rsp
-    cmp [rsi], byte 0	; compare rsi (source address) to null ptr (end of str)
-    jz end				; if null ptr = end of loop, jump to _end section and return
+	;push rbp
+	;mov rbp, rsp
+;    xor rax, rax
+;    push rdi
+	mov r12, rdi ; copier str (rdi) dans r12 (callee-saved register)
+	cmp rdi, 0
+	jz error
+	call ft_strlen ; rax = len de str (rsi)
+    inc rax ; +1 pour le \0
+	mov rdi, rax ; pour envoyer la taille à malloc
+	mov rax, 4
+	syscall
+	;	call _malloc
+	cmp rax, 0 ; si 0, malloc failed
+	jz error
+	mov rdi, rax ; malloc le 1er param de cpy
+	mov rsi, r12 ; str remis dans rsi (2eme param de cpy)
+;    mov rsi, rdi
+;    mov rdi, rax
+	call ft_strcpy ; effectue la copie de rsi vers rdi
+	;pop rbp
+	ret
 
-
-end:
-    pop rbp
-    ret
+error:
+		mov rax, 0
+	;	pop rbp
+		ret
--- srcs/ft_strdup_copy.s
+++ srcs/ft_strdup_copy.s
@@ -1,32 +1,24 @@
-;section .text
+section .text
 		global ft_strdup
+		extern malloc
 		extern ft_strlen
 		extern ft_strcpy
 
 ft_strdup:
-	push rbp
-	mov rbp, rsp
-	cmp [rbp], byte 0
-	jz _error_malloc
-	mov r12, rdi ; copier str (rdi) dans r12 (callee-saved register)
-	call ft_strlen ; rax = len de str (rsi)
-    inc rax ; +1 pour le \0
-	mov rdi, rax ; pour envoyer la taille à malloc
-	mov rax, 4
-	syscall
-	;	call _malloc
-	cmp rax, 0 ; si 0, malloc failed
-	jz _error_malloc
-	mov rdi, rax ; malloc le 1er param de cpy
-	mov rsi, r12 ; str remis dans rsi (2eme param de cpy)
-	call ft_strcpy ; effectue la copie de rsi vers rdi
-	pop rbp
-	ret
-
-;end:
-;		pop rbp
-;		ret
-
+		push rbp
+		mov rbp, rsp
+		mov r12, rdi ; copier str dans r10 (rdi car 1er arg) à mettre dans r12 (callee-saved register) (pourtant : r10 registre temporaire pour les ptrs sur strings)
+		call ft_strlen ; rax = len de str (rsi)
+        inc rax ; +1 pour le \0
+		mov rdi, rax ; pour envoyer la taille à malloc
+		call malloc
+		cmp rax, 0 ; si 0, malloc failed
+		jz _error_malloc
+		mov rdi, rax ; malloc le 1er param de cpy
+		mov rsi, r12 ; str remis dans rsi (2eme param de cpy)
+		call ft_strcpy ; effectue la copie de rsi vers rdi
+		pop rbp
+		ret
 
 _error_malloc:
 		mov rax, -1
